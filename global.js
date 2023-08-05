229c6100808e0300403853ceceb26547845d27a8
edimaron <88264893+edimaron@users.noreply.github.com>
2023-08-05 17:43:22 -0300

    Create global.js

---
+++ global.js
@@ -0,0 +1,7 @@
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  intent="WELCOME"
+  chat-title="NewAgent"
+  agent-id="8ea01cf7-6f08-49af-83f9-20400b940b85"
+  language-code="pt-br"
+></df-messenger>
