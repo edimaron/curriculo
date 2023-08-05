266a13e4a86a26cecabf9f19031cd7ddedd95479
edimaron <88264893+edimaron@users.noreply.github.com>
2023-08-05 18:02:59 -0300

    delete global.js

--- global.js
+++
@@ -1,7 +0,0 @@
-<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-<df-messenger
-  intent="WELCOME"
-  chat-title="NewAgent"
-  agent-id="8ea01cf7-6f08-49af-83f9-20400b940b85"
-  language-code="pt-br"
-></df-messenger>
